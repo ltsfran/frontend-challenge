cb7c475bc6f7d47b8e58b23d5e2dd81500c3ce06
Luis Tupa <luis.tupa@neoauto.pe>
2022-11-17 15:54:09 -0500

    feat: add layout component

--- pages/api/items.ts
+++ pages/api/items.ts
@@ -2,7 +2,7 @@ import { NextApiHandler } from 'next'
 
 const handler: NextApiHandler = (req, res) => {
   res.status(200).json({
-    message: 'hello'
+    message: 'items'
   })
 }
 
---
+++ pages/items/[id].tsx
@@ -0,0 +1,8 @@
+import { NextPage } from 'next'
+import Layout from '@components/Layout'
+
+const ItemPage: NextPage = () => (
+  <Layout>ItemPage</Layout>
+)
+
+export default ItemPage
