87f66176281ee8e22cfcf54970b9789825149c5d
Luis Tupa <luis.tupa@neoauto.pe>
2022-11-18 21:07:33 -0500

    feat: add webkit scrollbar in windows OS

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -18,11 +18,41 @@ const Content = styled.div`
   flex: 1;
   display: flex;
   flex-wrap: wrap;
-  flex-direction: column;
   width: 100%;
   height: 100%;
   padding: 1rem;
   box-sizing: border-box;
+
+  &::-webkit-scrollbar {
+    background-color: rgba(0,0,0,0);
+    width: 1rem;
+    height: 1rem;
+    z-index: 999999;
+  }
+
+  &::-webkit-scrollbar-track {
+    background-color: rgba(0,0,0,0);
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: rgba(0,0,0,0);
+    border-radius: 1rem;
+    border: 0px solid #FFFFFF;
+  }
+
+  &::-webkit-scrollbar-button {
+    display: none;
+  }
+
+  &:hover::-webkit-scrollbar-thumb {
+    background-color: #A0A0A5;
+    border: 4px solid #FFFFFF;
+  }
+
+  &::-webkit-scrollbar-thumb:hover {
+    background-color: #A0A0A5;
+    border: 4px solid #F4F4F4;
+  }
 
   @media (min-width: 600px) {
     max-width: 460px;
