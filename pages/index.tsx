20c4092b330f7de695a3b6086f0454f3d82f0f49
Luis Tupa <luis.tupa@neoauto.pe>
2022-11-18 22:33:36 -0500

    feat: add Card component in Items page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,16 +4,6 @@ import styled from 'styled-components'
 import Layout from '@components/Layout'
 import Search from '@components/Search'
 
-const Box = styled.div`
-  flex: 1;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  width: 100%;
-  height: 100%;
-  position: relative;
-`
-
 const Content = styled.div`
   position: absolute;
   transform: translateY(50%);
@@ -38,16 +28,18 @@ const SearchBox = styled.div`
 
 const IndexPage: NextPage = () => (
   <Layout>
-    <Box>
-      <Content>
-        <ImageWrapper>
-          <Image src="/logo.svg" alt="logo" width={160} height={160} />
-        </ImageWrapper>
-        <SearchBox>
-          <Search onEnter={(value) => console.log(value, 'value')} />
-        </SearchBox>
-      </Content>
-    </Box>
+    <Content>
+      <ImageWrapper>
+        <Image
+          src="/logo.svg"
+          alt="logo"
+          width={160}
+          height={160} />
+      </ImageWrapper>
+      <SearchBox>
+        <Search onEnter={(value) => console.log(value, 'value')} />
+      </SearchBox>
+    </Content>
   </Layout>
 )
 
