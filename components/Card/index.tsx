c93dbac625ab28130aa5f4e34cb39453a49f3840
Luis Tupa <luis.tupa@neoauto.pe>
2022-11-20 22:33:00 -0500

    feat: add character integration

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import {
   CardBox,
   CardImage,
@@ -8,6 +9,7 @@ import {
 } from './styled'
 
 interface CardProps {
+  to: string
   imageUrl: string
   title: string
   subTitle: string
@@ -15,26 +17,29 @@ interface CardProps {
 }
 
 const Card: React.FC<CardProps> = ({
+  to,
   imageUrl,
   title,
   subTitle,
   index = 0
 }) => (
-  <CardBox>
-    <CardImage>
-      <Image
-        src={imageUrl}
-        width={160}
-        height={160}
-        alt={`card-image-${index}`}
-        placeholder="blur"
-        blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mN8Vw8AAmEBb87E6jIAAAAASUVORK5CYII=" />
-    </CardImage>
-    <CardDescription>
-      <CardTitle>{title}</CardTitle>
-      <CardSubTitle>{subTitle}</CardSubTitle>
-    </CardDescription>
-  </CardBox>
+  <Link href={to}>
+    <CardBox>
+      <CardImage>
+        <Image
+          src={imageUrl}
+          width={160}
+          height={160}
+          alt={`card-image-${index}`}
+          placeholder="blur"
+          blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mN8Vw8AAmEBb87E6jIAAAAASUVORK5CYII=" />
+      </CardImage>
+      <CardDescription>
+        <CardTitle>{title}</CardTitle>
+        <CardSubTitle>{subTitle}</CardSubTitle>
+      </CardDescription>
+    </CardBox>
+  </Link>
 )
 
 export default Card
--- services/characters/index.ts
+++ services/characters/index.ts
@@ -1,23 +1,9 @@
-import { fetchAjax } from '@lib/fetch'
+import { getAllCharactersByQuery } from './characters'
+import { getCharacterById } from './character'
 
 const CharacterService = {
-  async getCharactersByQuery (query: string) {
-    try {
-      const REST_API = process.env.REST_API ?? ''
-      const response = await fetchAjax(`${REST_API}/api/items?q=${query}`, { method: 'GET' })
-      const data = await response.json()
-
-      return {
-        data: data.data,
-        error: undefined
-      }
-    } catch (error) {
-      if (error instanceof Error) {
-        console.error('Character service error::', error.message)
-      }
-      return { data: undefined, error: 'Internal server error' }
-    }
-  }
+  getAllCharactersByQuery,
+  getCharacterById
 }
 
 export default CharacterService
